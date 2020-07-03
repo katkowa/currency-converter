11088048b5937ef179c3ba3cfa1475b1d8e1d6f6
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-07-03 22:37:22 +0200

    functions in js added

--- js/script.js
+++ js/script.js
@@ -1,35 +1,39 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+    let formElement = document.querySelector(".js-form");
+    let amountElement = document.querySelector(".js-amount");
+    let currencyElement = document.querySelector(".js-currency");
+    let resultElement = document.querySelector(".js-result");
 
-let rateEUR = 4.4524;
-let rateUSD = 3.9667;
-let rateGBP = 4.9293;
-let rateKRW = 0.003269;
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+        let amount = +amountElement.value;
+        let currency = currencyElement.value;
+        let result = calculateResult(amount, currency);
 
-    let amount = +amountElement.value;
-    let rate;
-    let result = 0;
+        resultElement.innerHTML = showResult(amount, result, currency);
+    });
 
-    switch (currencyElement.value) {
-        case "EUR":
-            rate = rateEUR;
-            break;
-        case "USD":
-            rate = rateUSD;
-            break;
-        case "GBP":
-            rate = rateGBP;
-            break;
-        case "KRW":
-            rate = rateKRW;
-            break;
+    function showResult(amount, result, currency) {
+        return `${amount} PLN = <span class="form__result--value">${result.toFixed(2)} ${currency}</span>`;
     }
-    result = amount / rate;
 
-    resultElement.innerHTML = `${amount} PLN = <span class="form__result--value">${result.toFixed(2)} ${currencyElement.value}</span>`;
-})
+
+    function calculateResult(amount, currency) {
+        return amount / getCurrencyRate(currency);
+    }
+
+    function getCurrencyRate(rate) {
+        switch (rate) {
+            case "EUR":
+                return 4.4524;
+            case "USD":
+                return 3.9667;
+            case "GBP":
+                return 4.9293;
+            case "KRW":
+                return 0.003269;
+        }
+    }
+
+}
